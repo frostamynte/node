783e27fd372c5d18d426b2a4c7c564fe20e0946b
frostamynte <henrikaleksander@proton.me>
2025-03-18 16:44:24 +0100

    Update t3rn-balance.sh

--- t3rn-balance.sh
+++ t3rn-balance.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Requires foundry curl -L https://foundry.paradigm.xyz | bash
-declare -a wallet=("0xE4ebb1F829046Ec6425d0dD1E41a91A3f9B22824" "0x2Ed8F2c6F399A6B30dc30d62561fBfB6B54a7F3e")
+declare -a wallet=("0x2Ed8F2c6F399A6B30dc30d62561fBfB6B54a7F3e")
 declare -a rpc=(
 	"https://api.zan.top/arb-sepolia"
 	"https://base-sepolia.drpc.org"
@@ -15,7 +15,7 @@ func_balance () {
 	echo $value/10^18 | bc -l | awk '{printf "%.6f\n", $0}'
 }
 
-for i in "${wallet[1]}" ; do
+for i in "${wallet[0]}" ; do
 	echo "Checking balance for $i"
 	for network in "${rpc[@]}" ; do
 		echo $network
